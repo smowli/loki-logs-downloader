900c4b48f67951773e91461a4748002f211dd22c
Vojtech Smolar <76426556+vosmol@users.noreply.github.com>
2024-09-29 17:27:02 +0200

    fix: support cjs,esm,ts formats together

---
+++ npm-check/sdk.mjs
@@ -0,0 +1,13 @@
+import { download } from '@vosmol/loki-logs-downloader'
+
+async function main() {
+	await download({
+		config: {
+			lokiUrl: 'http://localhost:3100',
+			query: '{app="test"}',
+			coolDown: 1000,
+		},
+	});
+}
+
+main();
